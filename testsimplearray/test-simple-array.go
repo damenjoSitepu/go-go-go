5de7e44c319d392e725fb2e98fc53a36a1c9c3d8
Damenjo Sitepu <damenjos@thebigrichgroup.com>
2023-07-02 16:52:00 +0700

    Update: Slicing step

--- testsimplearray/test-simple-array.go
+++ testsimplearray/test-simple-array.go
@@ -1,18 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("Basic Array")
+	// fmt.Println("Basic Array")
 	var myNumber [2]int
 	myNumber[0] = 100
-	firstIndex := myNumber[1]
-	fmt.Println(firstIndex)
+	// firstIndex := myNumber[1]
+	// fmt.Println(firstIndex)
 
-	alphaGroup1 := [3]string{"Damenjo", "Tari", "Fadhil"}
-	fmt.Print(alphaGroup1)
-	alphaGroup2 := [...]string{"PHP", "Phyton", "Golang", "C++", "Java"}
-	fmt.Print(alphaGroup2)
+	// alphaGroup1 := [3]string{"Damenjo", "Tari", "Fadhil"}
+	// fmt.Print(alphaGroup1)
+	// alphaGroup2 := [...]string{"PHP", "Phyton", "Golang", "C++", "Java"}
+	// fmt.Print(alphaGroup2)
+	// letters := []string{"A", "B"}
+	// fmt.Print(letters)
+
+	// slice := make([]byte, 5, 5)
+	// fmt.Println(cap(slice))
+
+	// randInt := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// fmt.Print(randInt[3:5])
+	// fmt.Print(randInt[:2])
+	// fmt.Print(randInt[4:])
+	// fmt.Print(randInt[:])
 }
